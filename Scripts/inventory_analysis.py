7a71e34a53c1e3b55eab4ce33d4bccc4b4a2a901
Clare Isaacson <Clare.Isaacson@ibm.com>
2022-11-03 16:40:02 -0400

    update file structure and store data in .txt files

---
+++ Scripts/inventory_analysis.py
@@ -0,0 +1,237 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from pandas import ExcelWriter
+from os import listdir
+from os.path import isfile, join
+import re
+from time import strptime
+import random
+from openpyxl.utils import range_boundaries
+from collections import defaultdict, Counter
+import itertools
+import sys
+
+
+class InventoryRates():
+    def __init__(self, inv_type='cumulative', data_path = None, data_sheet = 0, date_col = None, issue_col = None,soh_col = None, window = 12, agg_cols = [],fac_cols=[],prod_cols=[]):
+        self.inv_type = inv_type
+        self.data_path = data_path
+        self.data_sheet = data_sheet
+        self.data_frame = pd.read_excel(self.data_path,sheet_name = self.data_sheet) if ('.xlsx' in self.data_path) else pd.read_csv(self.data_path)
+        self.data_frame = self.data_frame.rename(columns=lambda x: x.strip())
+        self.date_col = date_col
+        self.issue_col = issue_col
+        self.soh_col = soh_col
+        self.window = window
+        self.agg_cols = agg_cols
+        self.rates_data = pd.DataFrame()
+        self.stock_data = pd.DataFrame()
+        self.fac_cols = fac_cols
+        self.prod_cols = prod_cols
+    
+    @property
+    def inv_type(self):
+        return self._inv_type
+
+    @inv_type.setter
+    def inv_type(self, t):
+        if not (t in ['transactional', 'cumulative']): 
+            raise ValueError("Invalid inventory data type")
+        self._inv_type = t
+
+    @property
+    def data_frame(self):
+    	return self._data_frame
+    
+    @data_frame.setter
+    def data_frame(self, df):
+    	self._data_frame = df
+
+    @property
+    def date_col(self):
+    	return self._date_col
+
+    @date_col.setter
+    def date_col(self, d):
+    	if not d:
+    		raise ValueError(f"Date column not specified")
+    	if not (d in self.data_frame):
+    		raise ValueError(f"Column '{d}' is not in data")
+    	self._date_col = d
+    	self.data_frame[self.date_col] = pd.to_datetime(self.data_frame[self.date_col])
+    	# if cumulative, check that date does not occur multiple times for the same grouping
+
+    @property
+    def issue_col(self):
+    	return self._issue_col
+
+    @issue_col.setter
+    def issue_col(self, i):
+    	if not i:
+    		raise ValueError(f"Issue column not specified")
+    	if not (i in self.data_frame):
+    		raise ValueError(f"Column '{i}' is not in data")
+    	self._issue_col = i
+
+    @property
+    def soh_col(self):
+    	return self._soh_col
+
+    @soh_col.setter
+    def soh_col(self, s):
+    	if not s:
+    		raise ValueError(f"SOH column not specified")
+    	if not (s in self.data_frame):
+    		raise ValueError(f"Column '{s}' is not in data")
+    	self._soh_col = s
+
+    @property
+    def agg_cols(self):
+    	return self._agg_cols
+
+    @agg_cols.setter
+    def agg_cols(self, a):
+    	if not a:
+    		raise ValueError(f"Aggregation columns not specified")
+    	for col in a:
+    		if not (col in self.data_frame):
+    			raise ValueError(f"Column '{col}' is not in data")
+    	self._agg_cols = a
+
+    @property
+    def fac_cols(self):
+    	return self._fac_cols
+
+    @fac_cols.setter
+    def fac_cols(self, f):
+    	if not f:
+    		raise ValueError(f"Facility columns not specified")
+    	for col in f:
+    		if not (col in self.data_frame):
+    			raise ValueError(f"Column '{col}' is not in data")
+    	self._fac_cols = f
+
+    @property
+    def prod_cols(self):
+    	return self._prod_cols
+
+    @prod_cols.setter
+    def prod_cols(self, p):
+    	if not p:
+    		raise ValueError(f"Product columns not specified")
+    	for col in p:
+    		if not (col in self.data_frame):
+    			raise ValueError(f"Column '{col}' is not in data")
+    	self._prod_cols = p
+
+    @property
+    def window(self):
+    	return self._window
+
+    @window.setter
+    def window(self, w):
+    	#if not (len(w) == 2):
+    	#	raise ValueError("Invalid window - must have length 2 min and max")
+    	if not (type(w) == int): #and type(w[1] == int)):
+    		raise ValueError("Invalid window types - must be integer")
+    	#if not (w[0] < w[1]):
+    		#raise ValueError("Invalid window - min must be less than max")
+    	self._window = w
+
+    #@method
+    def rolling_rates(self):
+        for col in self.agg_cols:
+        	self.data_frame[col] = self.data_frame[col].map(str)
+        grouped = (self.data_frame.groupby(self.agg_cols+[self.date_col])
+                   .agg({self.issue_col:'sum', self.soh_col:'mean'})
+                   .groupby(level=0)
+                   .rolling(window=self.window,min_periods=self.window//2)
+                   .agg({self.issue_col:['std','mean','sum','count'],self.soh_col:'mean'}))
+        grouped.columns = grouped.columns.map('_'.join).str.strip('_')
+        grouped = grouped.reset_index(level=0).drop(self.fac_cols,axis=1).reset_index()
+        print(grouped)
+        #grouped = self.data_frame.groupby(self.agg_cols+[self.date_col]).agg({self.issue_col:'sum', self.soh_col:'mean'})
+        #print('grouped after groupby')
+        #print(grouped)
+        #grouped = grouped.rolling(window=self.window, min_periods=self.window)
+        #print('grouped after rolling')
+        #print(grouped)
+        #grouped = grouped.agg({self.issue_col:['std','mean','sum','count'],self.soh_col:'mean'}).reset_index(level=0).reset_index()
+        #print('grouped after agg')
+        #print(grouped)
+        #grouped.columns = grouped.columns.map('_'.join).str.strip('_')
+        grouped['Consumption_COV'] = grouped[self.issue_col+'_std']/grouped[self.issue_col+'_mean']
+        grouped['Inventory_turn'] = grouped[self.issue_col+'_sum']/grouped[self.soh_col+'_mean']
+        grouped.loc[~np.isfinite(grouped['Inventory_turn']), 'Inventory_turn'] = np.nan
+        grouped.loc[~np.isfinite(grouped['Consumption_COV']),'Consumption_COV'] = np.nan
+        self.rates_data = grouped
+
+    #@method
+    def stock_status(self):
+        
+        def get_blank(x):
+            return (pd.isnull(x[self.issue_col])) & (pd.isnull(x[self.soh_col]))
+        def group_last_soh(x):
+            d = {}
+            x['Blank'] = x.apply(get_blank,axis=1)
+            #d['Group'] = x[unique_group].sum(1)
+            x_non_blank = x[x['Blank'] == False]
+            if x_non_blank.shape[0] == 0:
+                d['Stock Status As Of'] = np.nan
+                d['Stock on Hand'] = np.nan
+            else:
+                d['Stock Status As Of'] = x_non_blank[self.date_col].iloc[-1]
+                d['Stock on Hand'] = np.nan if pd.isnull(x_non_blank[self.soh_col].iloc[-1]) else x_non_blank[self.soh_col].iloc[-1] 
+            x_cons = x[x[self.issue_col]>0]
+            if x_cons.shape[0] < 3:
+                d['AMC'] = x_cons[self.issue_col].mean()
+            else:
+                d['AMC'] = x_cons.iloc[[-3,-2,-1]][self.issue_col].mean()
+            d['% Records Blank'] = (x[x['Blank'] == True].shape[0])/x.shape[0]
+            return pd.Series(d,index=['Stock Status As Of','Stock on Hand','AMC','% Records Blank'])
+
+        df = self.data_frame.sort_values(by=[self.date_col])
+        print('before')
+        df = df.groupby([self.fac_cols[0],self.prod_cols[0]]).apply(lambda x: group_last_soh(x)).reset_index()
+        print('after')
+        df['MOS'] = np.where(df['AMC']==0,0,df['Stock on Hand']/df['AMC'])
+        df = df[[self.fac_cols[0],self.prod_cols[0],'Stock Status As Of','Stock on Hand','AMC','MOS','% Records Blank']]
+        self.stock_data = df
+
+    def transactional(self):
+        if self.inv_type == 'transactional':
+            self.data_frame[self.date_col] = self.data_frame[self.date_col].apply(lambda x: x.replace(day=1))
+
+    def format_columns(self):
+        names = {self.fac_cols[0]:'Facility_id',
+             self.prod_cols[0]:'Product_id',
+             self.date_col:'Date',
+             self.issue_col+'_std':'Consumption_std',
+             self.issue_col+'_mean':'Consumption_mean',
+             self.issue_col+'_sum':'Consumption_sum',
+             self.issue_col+'_count':'Consumption_count',
+             self.soh_col+'_mean':'SOH_mean'}
+        self.rates_data = self.rates_data.rename(columns=names)
+        self.rates_data = self.rates_data[list(names.values())+['Consumption_COV','Inventory_turn']]
+        self.stock_data = self.stock_data.rename(columns={self.fac_cols[0]:'Facility_id',self.prod_cols[0]:'Product_id'})
+
+    def clean_text(self):
+        for col1 in self.prod_cols:
+            self.data_frame[col1] = self.data_frame[col1].apply(lambda x: str(x).strip().upper())  
+        for col2 in self.fac_cols:
+            self.data_frame[col2] = self.data_frame[col2].apply(lambda x: str(x).strip().upper())
+
+    def create_ref_tables(self):
+        self.fac_table = self.data_frame[self.fac_cols].drop_duplicates(subset=self.fac_cols[0])
+        self.prod_table = self.data_frame[self.prod_cols].drop_duplicates(subset=self.prod_cols[0]).dropna()
+        self.plot_data = self.rates_data[['Facility_id','Product_id']].drop_duplicates()
+        self.dates_table = pd.DataFrame(sorted(list(self.rates_data['Date'].unique())),columns=['Date'])
+
+
+
+
+
+    
+    
+    
---
+++ Scripts/inventory_analysis_gui.py
@@ -0,0 +1,242 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import pandas as pd 
+import os
+from inventory_analysis import InventoryRates
+import configparser
+from os import path
+
+window = Tk()
+window.title("Inventory Analysis")
+window.geometry("800x700")
+
+
+import logging
+logging.basicConfig(filename=os.getcwd()+'/../Log/inventory_analysis.log',level=logging.DEBUG,format='%(asctime)s:%(levelname)s:%(message)s')
+
+class IAGui:
+	def __init__(self, window):
+		self.config = IntVar(value=0)
+		Radiobutton(window,text="Use saved settings",variable=self.config,value=1).grid(row=1,column=2,sticky=W)
+		Radiobutton(window,text="New configurations",variable=self.config,value=0).grid(row=1,column=3,sticky=W)
+
+		self.label_file_explorer = Label(window,text = "Select an inventory data file to begin",fg = "blue")
+		self.label_file_explorer.grid(row=2,column=2)
+
+		self.analysis = None
+
+		file_button = Button(window,
+						text = "Browse Files",
+						command = self.browseFiles)
+		file_button.grid(row=3,column=2)
+
+	def browseFiles(self):
+		
+		if self.config.get() == 1:
+			basepath = path.abspath("")
+
+			config_obj = configparser.ConfigParser()
+			config_obj.read(path.join(basepath,'configfile.ini'))
+
+			col_names = config_obj['column headers']
+			
+			self.data_type = StringVar(value=col_names['data_type'])
+			self.product = StringVar(value=col_names['product'])
+			self.product1 = StringVar(value=col_names['product1'])
+			self.product2  = StringVar(value=col_names['product2'])
+			self.facility = StringVar(value=col_names['facility'])
+			self.facility1 = StringVar(value=col_names['facility1'])
+			self.facility2 = StringVar(value=col_names['facility2'])
+			self.date_field = StringVar(value=col_names['date_field'])
+			self.issue_col = StringVar(value=col_names['issue_col'])
+			self.soh_col = StringVar(value=col_names['soh_col'])
+
+			other = config_obj['other']
+
+			self.rates_window = IntVar(value=int(other['rates_window']))
+			self.issue_0 = IntVar(value=int(other['issue_0']))
+			self.soh_0 = IntVar(value=int(other['soh_0']))
+
+			self.filename = ""
+			self.column_names = ["N/A"]
+			self.sheetname = StringVar()
+
+		else:
+
+			self.data_type = StringVar()
+			self.product = StringVar()
+			self.product1 = StringVar()
+			self.product2  = StringVar()
+			self.facility = StringVar()
+			self.facility1 = StringVar()
+			self.facility2 = StringVar()
+			self.date_field = StringVar()
+			self.rates_window = IntVar(value=12)
+			self.issue_col = StringVar()
+			self.issue_0 = IntVar(value=0)
+			self.soh_col = StringVar()
+			self.soh_0 = IntVar(value=0)
+			self.congif = IntVar(value=0)
+			self.filename = ""
+			self.column_names = ["N/A"]
+			self.sheetname = StringVar()
+
+		self.filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File", filetypes = (("Excel","*.xlsx"),("CSV","*.csv")))
+
+		if self.filename:
+
+			self.label_file_explorer.configure(text="File Opened: "+self.filename.split("/")[-1])
+			logging.info('File selected: {}'.format(self.filename))
+
+			if str(self.filename).endswith('.xlsx'):
+				#wb = openpyxl.load_workbook(self.filename)
+				#print(wb.sheetnames)
+				wb = pd.ExcelFile(self.filename)
+				#print(wb.sheet_names)
+				if len(wb.sheet_names) == 1:
+					self.sheetname.set(wb.sheet_names[0])
+				else:
+					Label(window, text="Sheet").grid(row=1, column=1,sticky=E)
+					ttk.Combobox(window, textvariable=self.sheetname, values=wb.sheet_names,justify=LEFT).grid(row=1,column=2, padx=(0,5))
+				self.column_names = [x.strip() for x in list(pd.read_excel(self.filename,sheet_name = self.sheetname.get(),nrows=1).columns)]
+			elif str(self.filename).endswith('.csv'):
+				self.column_names = [x.strip() for x in list(pd.read_csv(self.filename,nrows=1).columns)]
+
+			dataTypeLabel = Label(window, text="Data Type")
+			dataTypeLabel.grid(row=4, column=1, sticky=NE)
+			Label(window, text="Product").grid(row=5, column=1,sticky=E)
+			Label(window, text="(Additional Product Info)").grid(row=6,column=1,sticky=E)
+			Label(window, text="(Additional Product Info)").grid(row=7,column=1,sticky=NE)
+			Label(window, text="Facility").grid(row=8, column=1, sticky=E)
+			Label(window, text="(Additional Facility Info)").grid(row=9,column=1,sticky=E)
+			Label(window, text="(Additional Facility Info)").grid(row=10,column=1,sticky=NE)
+			Label(window, text="Date Field").grid(row=11, column=1, sticky=E)
+			Label(window, text="Consumption Field").grid(row=12, column=1, sticky=E)
+			Label(window, text="Stock on Hand Field").grid(row=13, column=1, sticky=NE)
+			Label(window, text="Time Window for Rates").grid(row=14, column=1, sticky=E)
+
+			#Label(window, text=None).grid(row=11, column=1, sticky=E)
+
+			data_type_entry = ttk.Combobox(window, textvariable=self.data_type, values=['transactional','cumulative'],
+				  justify=LEFT)
+			data_type_entry.grid(row=4,column=2, padx=(0,5),pady=(0,50))
+			#print(self.column_names)
+			ttk.Combobox(window, textvariable=self.product, values=self.column_names,
+				  justify=LEFT).grid(row=5,column=2, padx=(0,5))
+			ttk.Combobox(window, textvariable=self.product1, values=self.column_names,
+				  justify=LEFT).grid(row=6,column=2, padx=(40,0))
+			ttk.Combobox(window, textvariable=self.product2, values=self.column_names,
+				  justify=LEFT).grid(row=7,column=2, padx=(40,0),pady=(0,50))
+			ttk.Combobox(window, textvariable=self.facility, values=self.column_names,
+				  justify=LEFT).grid(row=8,column=2, padx=(0,5))
+			ttk.Combobox(window, textvariable=self.facility1, values=self.column_names,
+				  justify=LEFT).grid(row=9,column=2, padx=(40,0))
+			ttk.Combobox(window, textvariable=self.facility2, values=self.column_names,
+				  justify=LEFT).grid(row=10,column=2, padx=(40,0),pady=(0,50))
+			ttk.Combobox(window, textvariable=self.date_field, values=self.column_names,
+				  justify=LEFT).grid(row=11,column=2, padx=(0,5))
+			ttk.Combobox(window, textvariable=self.issue_col, values=self.column_names,
+				  justify=LEFT).grid(row=12,column=2, padx=(0,5))
+			Radiobutton(window,text="Blank fields = 0",variable=self.issue_0,value=1).grid(row=12,column=3,sticky=W,padx=(0,5))
+			Radiobutton(window,text="Blank fields are missing",variable=self.issue_0,value=0).grid(row=12,column=4,sticky=W,padx=(0,5))
+			ttk.Combobox(window, textvariable=self.soh_col, values=self.column_names,
+				  justify=LEFT).grid(row=13,column=2, padx=(0,5),pady=(0,50))
+			Radiobutton(window,text="Blank fields = 0",variable=self.soh_0,value=1).grid(row=13,column=3,sticky=W,padx=(0,5),pady=(0,50))
+			Radiobutton(window,text="Blank fields are missing",variable=self.soh_0,value=0).grid(row=13,column=4,sticky=W,padx=(0,5),pady=(0,50))
+			ttk.Combobox(window, textvariable=self.rates_window, values=list(range(6,25)),
+				  justify=LEFT).grid(row=14,column=2, padx=(0,5))
+
+			run_button = Button(window,
+								text="Run Analysis",
+								command = self.runIA)
+			run_button.grid(row=15,column=2)
+	
+	def runIA(self):
+		print("running")
+		products = list(filter(None,[self.product.get(),self.product1.get(),self.product2.get()]))
+		facilities = list(filter(None,[self.facility.get(),self.facility1.get(),self.facility2.get()]))
+		i = InventoryRates(inv_type = self.data_type.get(), data_path = self.filename, date_col = self.date_field.get(), issue_col = self.issue_col.get(), soh_col = self.soh_col.get(), window= self.rates_window.get(),agg_cols=facilities+products,fac_cols = facilities, prod_cols=products)
+		Label(window, text='Calculating Rates').grid(row=16,column=2)
+		logging.debug('Product columns: {}'.format(products))
+		logging.debug('Facility columns: {}'.format(facilities))
+		logging.info('Inventory rates object instantiated')
+		i.clean_text()
+		i.transactional()
+		print("being rolling rates")
+		i.rolling_rates()
+		print("end rolling rates")
+		i.stock_status()
+		print("end stock status")
+		i.format_columns()
+		print("end format cols")
+		i.create_ref_tables()
+		print("end create ref tables")
+		self.analysis = i
+
+		#write files
+		print('executing')
+		self.analysis.rates_data.to_csv('rates_data.txt', index=None, sep=' ', mode='w')
+		self.analysis.stock_data.to_csv('stock_data.txt', index=None, sep=' ', mode='w')
+		self.analysis.prod_table.to_csv('product_list.txt', index=None, sep=' ', mode='w')
+		self.analysis.fac_table.to_csv('facility_list.txt', index=None, sep=' ', mode='w')
+		self.analysis.plot_data.to_csv('plot_data.txt', index=None, sep=' ', mode='w')
+		self.analysis.dates_table.to_csv('dates_table.txt', index=None, sep=' ', mode='w')
+		print('finished')
+
+		logging.info('Inventory rates calculated')
+
+		#display results
+		Label(window, text='Rates Calculated').grid(row=16,column=2)
+		report_button = Button(window,
+							text="Open Report in Excel",
+							command = self.openReport)
+		report_button.grid(row=17,column=2)
+		num_prods = "Total Products: "+str(i.prod_table.shape[0])
+		num_facs = "Total Facilities: "+str(i.fac_table.shape[0])
+		Label(window, text=num_prods).grid(row=18,column=2)
+		Label(window, text=num_facs).grid(row=19,column=2)
+		config_button = Button(window,
+							text="Save My Settings",
+							command = self.createConfig)
+		config_button.grid(row = 20,column=2)
+
+	def openReport(self):
+		
+		os.system("start excel.exe ..\LMIS_Analysis_Report.xlsm")
+		logging.info('Report created')
+
+	def openSettings(self):
+		# Settings panel where user can set directory for output & other settings
+		window2=Tk()
+		window2.title("Inventory Analysis Settings")
+
+	def createConfig(self):
+		config = configparser.ConfigParser()
+		config.add_section('column headers')
+		config.set('column headers','data_type', self.data_type.get())
+		config.set('column headers','product',self.product.get())
+		config.set('column headers','product1',self.product1.get())
+		config.set('column headers','product2',self.product2.get())
+		config.set('column headers','facility',self.facility.get())
+		config.set('column headers','facility1',self.facility1.get())
+		config.set('column headers','facility2',self.facility2.get())
+		config.set('column headers','date_field',self.date_field.get())
+		config.set('column headers','issue_col',self.issue_col.get())
+		config.set('column headers','soh_col',self.soh_col.get())
+
+		config.add_section('other')
+		config.set('other','rates_window',str(self.rates_window.get()))
+		config.set('other','issue_0',str(self.issue_0.get()))
+		config.set('other','soh_0',str(self.soh_0.get()))
+
+		basepath = path.abspath("")
+
+		with open(path.join(basepath,'configfile.ini'),'w') as configfile:
+			config.write(configfile)
+
+
+IAGui(window)
+window.mainloop()
+
+
